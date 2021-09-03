5d10d98570fae6a87fe00ebffb3e1972a99bfff2
santiagoms2404 <santiagomarinsuarez@me.com>
2021-09-03 12:23:50 -0500

    First commit to GitHub

---
+++ first.cpp
@@ -0,0 +1,18 @@
+/*
+Programmer : Santiago Marin Suarez
+*Course : CPTR 212
+*Description : first program to be pushed to GitHub
+*Indentation style: Allman style 
+* Date : September 3, 2021
+*Last changed : September 3, 2021, 11:03 AM
+*/
+
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout <<"Hello world" << std::endl;
+    return 0;
+}
